78c74d1fd1f9c0d5d409eb7cb031e0f933dff06f
iadson <i.vini@hotmail.com>
2026-04-22 20:43:50 -0300

    Primeiro commit

---
+++ src/main/java/br/com/ensinogameficado/EnsinogameficadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ensinogameficado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsinogameficadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsinogameficadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ensinogameficado/EnsinogameficadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ensinogameficado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsinogameficadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
